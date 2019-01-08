436a41c1ca7315732d5758d6c5c82c5aaef303ef
AviaTorX <shinde91avinash@gmail.com>
2019-01-09 05:13:57 +0530

    webpack configuration file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var path = require("path");
+var webpack = require('webpack');
+
 var config = {
     mode:"none",
     entry:"./src/index.tsx",
     output:{
         path: path.resolve(__dirname,"dist"),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: '/'
     },
     resolve:{
         extensions:[".ts", ".tsx", ".js", ".css"]
@@ -47,7 +50,21 @@ var config = {
             filename: "[name].css",
             chunkFilename: "[id].css"
           }),
-    ]
+    ],
+    devServer:{
+      historyApiFallback: true
+    }
 };
+
+if(process.env.NODE_ENV === 'production'){
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env' : {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      } 
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
 
 module.exports = config;
